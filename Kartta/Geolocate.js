01fb8f483fb733892ae49acf1cb0aa9d8cdd8229
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-01-10 23:00:36 +0200

    Update Geolocate.js

--- Kartta/Geolocate.js
+++ Kartta/Geolocate.js
@@ -2,7 +2,6 @@ let userMarker; // Käyttäjän sijaintia varten
 let watchID;
 let isTracking = false;
 
-// Määritellään ikoni käyttäjän sijainnille
 let userIcon = L.icon({
     iconUrl: 'circle-icon.png',
     iconSize: [24, 24],
@@ -18,37 +17,40 @@ function updateUserLocation(lat, lon) {
 }
 
 function startTracking() {
+    // Jos seuranta on jo päällä, lopeta se ja poista merkki
     if (isTracking) {
-        // Lopeta käyttäjän sijainnin seuraaminen, mutta älä poista merkkiä
         navigator.geolocation.clearWatch(watchID);
         isTracking = false;
-        document.querySelector('#locateUser img').src = "locate.png";
-    } else {
-        // Aloita käyttäjän sijainnin seuraaminen
-        if ("geolocation" in navigator) {
-            isTracking = true;
-            document.querySelector('#locateUser img').src = "locate-active.png";
-            watchID = navigator.geolocation.watchPosition(function(position) {
-                const lat = position.coords.latitude;
-                const lon = position.coords.longitude;
-
-                updateUserLocation(lat, lon);
-                map.setView([lat, lon], 16);
-            }, function(error) {
-                console.error("Sijainnin seuranta epäonnistui: ", error);
-                isTracking = false;
-            }, {
-                enableHighAccuracy: true,
-                maximumAge: 10000,
-                timeout: 5000
-            });
-        } else {
-            alert("Selaimesi ei tue sijainnin hakua.");
+        if (userMarker) {
+            map.removeLayer(userMarker);
+            userMarker = null;
         }
+        document.querySelector('#locateUser img').src = "locate.png";
+        return; // Keskeytä funktion suoritus
+    }
+
+    // Aloita käyttäjän sijainnin seuraaminen
+    if ("geolocation" in navigator) {
+        isTracking = true;
+        document.querySelector('#locateUser img').src = "locate-active.png";
+        watchID = navigator.geolocation.watchPosition(function(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            updateUserLocation(lat, lon);
+            map.setView([lat, lon], 16);
+        }, function(error) {
+            console.error("Sijainnin seuranta epäonnistui: ", error);
+            isTracking = false;
+        }, {
+            enableHighAccuracy: true,
+            maximumAge: 10000,
+            timeout: 5000
+        });
+    } else {
+        alert("Selaimesi ei tue sijainnin hakua.");
     }
 }
 
-// Kun karttaa liikutetaan, lopeta automaattinen seuraaminen, mutta älä poista merkkiä
 map.on('dragstart', function() {
     if (isTracking) {
         navigator.geolocation.clearWatch(watchID);
