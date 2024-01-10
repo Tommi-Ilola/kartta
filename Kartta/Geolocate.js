49fd25f75a3c2982bffe6fa93a12bae72919aa36
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-01-10 21:13:16 +0200

    Update Geolocate.js

--- Kartta/Geolocate.js
+++ Kartta/Geolocate.js
@@ -1,3 +1,66 @@
+(function() {
+    // save these original methods before they are overwritten
+    var proto_initIcon = L.Marker.prototype._initIcon;
+    var proto_setPos = L.Marker.prototype._setPos;
+
+    var oldIE = (L.DomUtil.TRANSFORM === 'msTransform');
+
+    L.Marker.addInitHook(function () {
+        var iconOptions = this.options.icon && this.options.icon.options;
+        var iconAnchor = iconOptions && this.options.icon.options.iconAnchor;
+        if (iconAnchor) {
+            iconAnchor = (iconAnchor[0] + 'px ' + iconAnchor[1] + 'px');
+        }
+        this.options.rotationOrigin = this.options.rotationOrigin || iconAnchor || 'center bottom' ;
+        this.options.rotationAngle = this.options.rotationAngle || 0;
+
+        // Ensure marker keeps rotated during dragging
+        this.on('drag', function(e) { e.target._applyRotation(); });
+    });
+
+    L.Marker.include({
+        _initIcon: function() {
+            proto_initIcon.call(this);
+        },
+
+        _setPos: function (pos) {
+            proto_setPos.call(this, pos);
+            this._applyRotation();
+        },
+
+        _applyRotation: function () {
+            if(this.options.rotationAngle) {
+                this._icon.style[L.DomUtil.TRANSFORM+'Origin'] = this.options.rotationOrigin;
+
+                if(oldIE) {
+                    // for IE 9, use the 2D rotation
+                    this._icon.style[L.DomUtil.TRANSFORM] = 'rotate(' + this.options.rotationAngle + 'deg)';
+                } else {
+                    // for modern browsers, prefer the 3D accelerated version
+                    this._icon.style[L.DomUtil.TRANSFORM] += ' rotateZ(' + this.options.rotationAngle + 'deg)';
+                }
+            }
+        },
+
+        setRotationAngle: function(angle) {
+            this.options.rotationAngle = angle;
+            this.update();
+            return this;
+        },
+
+        setRotationOrigin: function(origin) {
+            this.options.rotationOrigin = origin;
+            this.update();
+            return this;
+        }
+    });
+})();
+
+// Varmista, että L.RotatedMarker on ladattu ja käytettävissä
+if (!L.RotatedMarker) {
+    console.error("L.RotatedMarker-kirjastoa ei löydy. Varmista, että olet lisännyt sen.");
+}
+
 let userMarker; // Käyttäjän sijaintia varten
 let headingMarker; // Suuntaa osoittavaa nuolta varten
 let watchID;
@@ -8,14 +71,12 @@ let userIcon = L.icon({
     iconUrl: 'circle-icon.png',
     iconSize: [24, 24],
     iconAnchor: [12, 12]
-	className: 'marker-icon'
 });
 
 let headingIcon = L.icon({
     iconUrl: 'arrow-icon.png',
     iconSize: [24, 24],
-    iconAnchor: [12, 12],
-    className: 'heading-icon' // Määritellään erillinen luokka suunnalle
+    iconAnchor: [12, 12]
 });
 
 function updateUserLocation(lat, lon) {
@@ -28,14 +89,13 @@ function updateUserLocation(lat, lon) {
 
 function updateHeading(lat, lon, heading) {
     if (!headingMarker) {
-        headingMarker = L.marker([lat, lon], {icon: headingIcon}).addTo(map);
+        headingMarker = new L.RotatedMarker([lat, lon], {
+            icon: headingIcon,
+            rotationAngle: heading // Asetetaan alkuperäinen kulma
+        }).addTo(map);
     } else {
         headingMarker.setLatLng([lat, lon]);
-    }
-
-    if (heading !== null && heading !== undefined && headingMarker._icon) {
-        // Käytä suoraa transformaatiota CSS:n kautta
-        headingMarker._icon.style.transform = 'rotate(' + heading + 'deg)';
+        headingMarker.setRotationAngle(heading);
     }
 }
 
@@ -45,47 +105,47 @@ function startTracking() {
         isTracking = false;
         if (userMarker) userMarker.remove();
         userMarker = null;
+        if (headingMarker) headingMarker.remove();
+        headingMarker = null;
 
-        // Muuta painikkeen kuvaa
         document.querySelector('#locateUser img').src = "locate.png";
     } else {
         if ("geolocation" in navigator) {
             isTracking = true;
             document.querySelector('#locateUser img').src = "locate-active.png";
-			watchID = navigator.geolocation.watchPosition(function(position) {
-				const lat = position.coords.latitude;
-				const lon = position.coords.longitude;
-				const heading = position.coords.heading;
+            watchID = navigator.geolocation.watchPosition(function(position) {
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+                const heading = position.coords.heading || 0;
 
-				updateUserLocation(lat, lon);
-				updateHeading(lat, lon, heading);
+                updateUserLocation(lat, lon);
+                updateHeading(lat, lon, heading);
 
-				if (isTracking) {
-					map.setView([lat, lon], 16);
-				}
-			}, function(error) {
-				console.error("Sijainnin seuranta epäonnistui: ", error);
-				isTracking = false;
-			}, {
-				enableHighAccuracy: true,
-				maximumAge: 10000,
-				timeout: 5000
-			});
+                map.setView([lat, lon], 16);
+            }, function(error) {
+                console.error("Sijainnin seuranta epäonnistui: ", error);
+                isTracking = false;
+            }, {
+                enableHighAccuracy: true,
+                maximumAge: 10000,
+                timeout: 5000
+            });
         } else {
             alert("Selaimesi ei tue sijainnin hakua.");
         }
     }
 }
 
-// Kartan liikuttaminen lopettaa käyttäjän seurannan
 map.on('dragstart', function() {
     if (isTracking) {
         navigator.geolocation.clearWatch(watchID);
         isTracking = false;
         if (userMarker) userMarker.remove();
         userMarker = null;
-        // Muuta painikkeen tekstiä tai ulkoasua tarvittaessa
-        document.querySelector('#locateUser img').src = "locate-active.png";
+        if (headingMarker) headingMarker.remove();
+        headingMarker = null;
+
+        document.querySelector('#locateUser img').src = "locate.png";
     }
 });
 
