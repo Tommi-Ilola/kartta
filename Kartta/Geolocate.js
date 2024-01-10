12cecc94c924fd777a4fbd3f1bdb09b00760eee0
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-01-10 22:13:25 +0200

    Update Geolocate.js

--- Kartta/Geolocate.js
+++ Kartta/Geolocate.js
@@ -1,11 +1,8 @@
-let userMarker; // Globaali muuttuja käyttäjän merkkiä varten
-let userHeading; // Globaali muuttuja käyttäjän suunnalle
+let userMarker; // Käyttäjän sijaintia varten
+let headingMarker; // Suuntaa osoittavaa nuolta varten
 
-function updateMarker(lat, lon, heading) {
-    // Päivitä käyttäjän sijaintia osoittavaa merkkiä
-    if (userMarker) {
-        userMarker.setLatLng([lat, lon]);
-    } else {
+function updateUserLocation(lat, lon) {
+    if (!userMarker) {
         userMarker = L.circleMarker([lat, lon], {
             radius: 8,
             fillColor: "#3186cc",
@@ -14,13 +11,24 @@ function updateMarker(lat, lon, heading) {
             opacity: 1,
             fillOpacity: 0.8
         }).addTo(map);
+    } else {
+        userMarker.setLatLng([lat, lon]);
+    }
+}
+
+function updateHeading(lat, lon, heading) {
+    if (!headingMarker) {
+        headingMarker = L.marker([lat, lon], {
+            icon: headingIcon
+        }).addTo(map);
+    } else {
+        headingMarker.setLatLng([lat, lon]);
     }
 
-    // Päivitä popup sisältämään ilmansuunta, jos se on saatavilla
+    // Päivitä käyttäjän sijaintia osoittavan merkin tooltip
     let popupContent = "Olet tässä: " + lat.toFixed(5) + ", " + lon.toFixed(5);
     if (heading !== undefined) {
         popupContent += "<br>Ilmansuunta: " + heading.toFixed(1) + "°";
-        userHeading = heading;
     }
     userMarker.bindPopup(popupContent).openPopup();
 }
@@ -30,11 +38,10 @@ function startTracking() {
         navigator.geolocation.watchPosition(function(position) {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
-            const heading = position.coords.heading; // Suunta, johon laite osoittaa, astetta (0 = pohjoinen)
-            
-            updateMarker(lat, lon, heading);
-            
-            // Zoomaa ja keskitä kartta uuteen sijaintiin
+            const heading = position.coords.heading;
+
+            updateUserLocation(lat, lon);
+            updateHeading(lat, lon, heading);
             map.setView([lat, lon], map.getZoom());
         }, function(error) {
             console.error("Sijainnin seuranta epäonnistui: ", error);
@@ -49,3 +56,4 @@ function startTracking() {
 }
 
 document.getElementById('locateUser').addEventListener('click', startTracking);
+
