8be1ab978db28604405f089bbd082c590c61eb30
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-01-16 20:17:29 +0200

    Update mapsetup.js

--- Kartta/mapsetup.js
+++ Kartta/mapsetup.js
@@ -39,11 +39,11 @@ proj4.defs("EPSG:3067", "+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs");
 
 // Oletetaan, että nämä ovat alkuperäiset koordinaatit ja zoomaus desktop-laitteille
 let defaultCoords = [64.615565, 26.484516];
-let defaultZoom = 5;
+let defaultZoom = 6;
 
 // Määritellään toiset koordinaatit ja zoomaus mobiililaitteille
-let mobileCoords = [64.532748, 25.579318]; // Esimerkiksi toiset koordinaatit
-let mobileZoom = 6;
+let mobileCoords = [67.532748, 25.579318]; // Esimerkiksi toiset koordinaatit
+let mobileZoom = 5;
 
 // Funktio laitetunnistukseen
 function isMobileDevice() {
