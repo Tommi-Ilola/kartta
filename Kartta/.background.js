cf6d045d14f74b0094c90d938a7e4d11d54f1b56
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-02-08 16:09:45 +0200

    Update .background.js

--- Kartta/.background.js
+++ Kartta/.background.js
@@ -1,22 +1,10 @@
-// Esimerkki tiedostosta: pages/api/my-background-task.background.js
-
 export default async (req, res) => {
-  // Tässä voit suorittaa pitkäkestoisia tehtäviä, kuten API-kutsuja
-  console.log('Taustatehtävä aloitettu.');
-
-  // Oletetaan, että sinulla on jokin funktio, joka suorittaa API-kutsun:
-  const data = await fetchSomeData();
-
-  // Kun tehtävä on suoritettu, voit tallentaa tulokset tietokantaan,
-  // lähettää sähköposti-ilmoituksen, jne.
-
-  // Vastaus lähetetään välittömästi, ilmoittaen että tehtävä on vastaanotettu.
-  res.status(200).json({ message: 'Tehtävä on vastaanotettu ja suoritetaan taustalla.' });
+  try {
+    const response = await fetch('https://rata.digitraffic.fi/infra-api/0.7/radat.geojson');
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Virhe ladattaessa radat.geojson dataa:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
-
-async function fetchSomeData() {
-  // Esimerkki API-kutsusta
-  const response = await fetch('https://rata.digitraffic.fi');
-  const data = await response.json();
-  return data;
-}
