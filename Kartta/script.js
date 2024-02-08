9af49e801f77aaa4819528a55916bd8964a11696
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-02-08 16:11:23 +0200

    Update script.js

--- Kartta/script.js
+++ Kartta/script.js
@@ -6,7 +6,7 @@ let currentResultNumber = 1;
 
 function haeKaikkiRatanumerot() {
     naytaDatanLatausIndikaattori();
-    fetch('/api/fetch-ratanumerot') // Käytä nyt taustafunktiota
+    fetch('/fetch-ratanumerot') // Käytä nyt taustafunktiota
         .then(response => response.json())
         .then(data => {
             ratanumerot = data.features.map(feature => feature.properties.ratanumero);
