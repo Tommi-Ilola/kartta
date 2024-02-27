6333d00548113738ee85523be03ee58f946b823c
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-02-27 18:09:20 +0200

    Delete netlify/functions directory

--- netlify/functions/api-proxy.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-exports.handler = async function(event) {
-  const response = await axios.get('URL', { tässä voisi olla konfiguraatiosi });
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response.data)
-  };
-};
