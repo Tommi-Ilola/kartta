91ef6f8af1242e202420b37a7ca267c0451cbca4
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-02-27 18:48:05 +0200

    Update hakuProxy.js

--- Kartta/hakuProxy.js
+++ Kartta/hakuProxy.js
@@ -1,9 +1,11 @@
-const axios = require('axios');
 exports.handler = async function(event) {
   try {
     const response = await axios.get('https://rata.digitraffic.fi/infra-api/0.7/radat.geojson');
     return {
       statusCode: 200,
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(response.data)
     };
   } catch (error) {
