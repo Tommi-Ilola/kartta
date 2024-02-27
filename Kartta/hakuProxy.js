5edc9e00777dda17bf99db7cc74f68a818f02d48
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-02-27 19:06:21 +0200

    Update hakuProxy.js

--- Kartta/hakuProxy.js
+++ Kartta/hakuProxy.js
@@ -1,14 +1,30 @@
-exports.handler = async function(event) {
-  try {
-    const response = await axios.get('https://rata.digitraffic.fi/infra-api/0.7/radat.geojson');
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(response.data)
-    };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
-  }
+const https = require('https');
+
+exports.handler = async function(event, context) {
+  return new Promise((resolve, reject) => {
+    const url = 'https://rata.digitraffic.fi/infra-api/0.7/radat.geojson';
+
+    https.get(url, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: data
+        });
+      });
+    }).on('error', (error) => {
+      reject({
+        statusCode: 500,
+        body: "Error calling the external API"
+      });
+    });
+  });
 };
