9c3dcceef00238b66703ace54831b8fa31587f5c
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-04-18 17:36:33 +0300

    Update Geolocate.js

--- public/Geolocate.js
+++ public/Geolocate.js
@@ -1,8 +1,7 @@
-var lc = L.control.locate({
+L.control.locate({
     locateOptions: {
          maxZoom: 17, // Suurin zoomaustaso, kun sijainti löydetään
          watch: true,  // Jatkuva paikannus kunnes käyttäjä lopettaa sen
-         setView: true, // Automaattisesti keskitä näkymä löydettyyn sijaintiin
-         keepCurrentZoomLevel: false
+         setView: true // Automaattisesti keskitä näkymä löydettyyn sijaintiin
     }
 }).addTo(map);
