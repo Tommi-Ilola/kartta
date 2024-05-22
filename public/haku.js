1226e88282b898ef7d404f8274879d8d46b22999
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-05-22 08:45:48 +0300

    Update haku.js

--- public/haku.js
+++ public/haku.js
@@ -41,29 +41,21 @@ function loadAnotherGeoJsonData() {
         .then(data => {
             globalAnotherGeoJsonData = data;
             console.log('Toinen GeoJSON data ladattu:', globalAnotherGeoJsonData);
-            // Voit yhdistää datan tässä vaiheessa tai suorittaa koordinaattimuunnoksen tarvittaessa
         })
         .catch(error => console.error('Virhe ladattaessa toista GeoJSON-tiedostoa:', error));
 }
 
-// Kutsu molempia funktioita ladataksesi datan
-loadGeoJsonData();
 loadAnotherGeoJsonData();
 
 function combineAllGeoJsonData() {
     if (globalGeoJsonData && globalAnotherGeoJsonData && globalThirdGeoJsonData) {
-        // Yhdistä kaikki features kolmesta datasetistä
         var combinedFeatures = globalGeoJsonData.features
             .concat(globalAnotherGeoJsonData.features)
             .concat(globalThirdGeoJsonData.features);
         globalGeoJsonData.features = combinedFeatures;
         console.log('Kaikki GeoJSON datasetit yhdistetty:', globalGeoJsonData);
-        // Tässä voit suorittaa muita toimenpiteitä yhdistetyn datan kanssa
     }
 }
-
-// Kutsu combineGeoJsonData-funktiota sen jälkeen, kun molemmat datasetit on ladattu
-
 
 function loadAllGeoJsonData() {
     Promise.all([
@@ -75,6 +67,7 @@ function loadAllGeoJsonData() {
             type: "FeatureCollection",
             features: [].concat(...datas.map(data => data.features))
         };
+        console.log('Kaikki GeoJSON datasetit ladattu:', globalGeoJsonData);
     }).catch(error => {
         console.error('Virhe ladattaessa GeoJSON-tietoja:', error);
     });
@@ -82,147 +75,64 @@ function loadAllGeoJsonData() {
 
 loadAllGeoJsonData();
 
-    var customIcon = L.icon({
-	  className: 'tasoristeys-haku',
-      iconUrl: 'tasoristeys1.png', // Markerin kuvatiedoston polku
-      iconSize: [36, 36], // Kuvan koko pikseleinä
-      iconAnchor: [20, 17], // Kuvan ankkuripiste, joka vastaa markerin sijaintia kartalla
-      tooltipAnchor: [1, -10]
-    });
-		
-function searchLocation(searchTerm) {
-    var searchTerm = document.getElementById('searchInput').value.trim();
-
-    if (searchTerm.length > 0 && globalGeoJsonData) {
-        var filteredData = {
-            type: "FeatureCollection",
-            features: globalGeoJsonData.features.filter(function(feature) {
-                return feature.properties.nimi.toLowerCase().includes(searchTerm.toLowerCase());
-            })
-        };
-
-        if (filteredData.features.length > 0) {
-            if (currentLayer) map.removeLayer(currentLayer);
-            currentLayer = L.geoJSON(filteredData, {
-                pointToLayer: function(feature, latlng) {
-                    // Tässä käytetään L.marker kustomoidun ikonin kanssa jokaiselle hakutulokselle
-                    return L.marker(latlng, {icon: customIcon});
-                }
-            }).addTo(map);
-            map.fitBounds(currentLayer.getBounds());
-            isSearchActive = true;
-            showCloseIcon();
-        } else {
-		
-        }
-    } else {
-        console.error('Hakukenttä on tyhjä tai dataa ei ole vielä ladattu.');
-        naytaVirheilmoitus('Hakukenttä on tyhjä tai dataa ei ole vielä ladattu.');
-        showMagnifierIcon();
-    }
-}
-
-// Funktiot määritellään ensin
-function style(feature) {
-    if (feature.geometry.type === 'MultiLineString') {
-		return {
-			color: "blue", // Sininen sisäväri
-			weight: 8,
-			opacity: 1
-		};
-    }
-}
-
-    // Käy läpi GeoJSON-datan etsien vastaavuutta
-	function convertCoordinates(feature) {
-		if (feature.geometry.type === 'MultiLineString') {
-			feature.geometry.coordinates = feature.geometry.coordinates.map(line => 
-				line.map(point => proj4(sourceProjection, destinationProjection, point))
-			);
-		} else if (feature.geometry.type === 'MultiPoint') {
-			feature.geometry.coordinates = feature.geometry.coordinates.map(point => 
-				proj4(sourceProjection, destinationProjection, point)
-			);
-		}
-		// Lisää tähän käsittely muille geometriatyypeille, kuten 'Point', 'Polygon', jne.
-	}
-
-Promise.all([
-    fetch(geojsonUrl).then(response => response.json()),
-    fetch(anotherGeojsonUrl).then(response => response.json()),
-    fetch(thirdGeojsonUrl).then(response => response.json())
-]).then(datas => {
-    // Yhdistetään kaikki ladatut datasetit yhteen FeatureCollectioniin
-    var combinedGeoJsonData = {
-        type: "FeatureCollection",
-        features: [].concat(...datas.map(data => data.features))
-    };
-
-    // Suoritetaan koordinaattimuunnokset
-    combinedGeoJsonData.features.forEach(convertCoordinates);
-
-    // Piirretään yhdistetty data kartalle
-    globalGeoJsonData = combinedGeoJsonData;
-
-}).catch(error => {
-    console.error('Virhe ladattaessa GeoJSON-tietoja:', error);
+var siltaIcon = L.icon({
+    iconUrl: 'silta.png',
+    iconSize: [36, 36],
+    iconAnchor: [20, 17],
+    tooltipAnchor: [1, -10]
 });
 
+var tasoristeysIcon = L.icon({
+    className: 'tasoristeys-haku',
+    iconUrl: 'tasoristeys1.png',
+    iconSize: [36, 36],
+    iconAnchor: [20, 17],
+    tooltipAnchor: [1, -10]
+});
+
+function pointToLayer(feature, latlng) {
+    let icon;
+    if (feature.properties.tyyppi === 'silta') {
+        icon = siltaIcon;
+    } else {
+        icon = tasoristeysIcon;
+    }
+    return L.marker(latlng, { icon: icon });
+}
+
+function style(feature) {
+    if (feature.geometry.type === 'MultiLineString') {
+        return {
+            color: "blue",
+            weight: 8,
+            opacity: 1
+        };
+    }
+}
+
 function drawGeoJsonOnMap(geoJsonData) {
-    // Poistetaan aikaisemmat hakutulokset kartalta
     if (window.searchResultsLayer) {
         map.removeLayer(window.searchResultsLayer);
     }
 
-    // Piirretään hakutulokset kartalle
     window.searchResultsLayer = L.geoJSON(geoJsonData, {
         onEachFeature: onEachFeature,
-        pointToLayer: function(feature, latlng) {
-            return L.marker(latlng, {
-                icon: L.icon({
-                    iconUrl: 'tasoristeys.png', // Osoita oikea polku ikonitiedostoosi
-					iconSize: [30, 30], // Kuvan koko pikseleinä
-					iconAnchor: [17, 14], // Kuvan ankkuripiste, joka vastaa markerin sijaintia kartalla
-					tooltipAnchor: [1, -10], // Popup-ikkunan sijainti suhteessa markerin ankkuriin
-                })
-            });
-        },
-		style: style
+        pointToLayer: pointToLayer,
+        style: style
     }).addTo(map);
-	
-
-	L.geoJSON(geoJsonData, {
-		style: function(feature) {
-			if (feature.geometry.type === 'MultiLineString') {
-				return {
-					color: "#00a8f3", // Sininen sisäväri
-					weight: 3,
-					opacity: 0
-				};
-			}
-		},
-		pointToLayer: pointToLayer
-	}).addTo(map);
-
-// Funktio, joka ajetaan jokaiselle featurelle, kun se lisätään kartalle
-function onEachFeature(feature, layer) {
-    if (feature.properties && feature.properties.nimi) {
-        layer.bindTooltip(feature.properties.nimi, {
-            permanent: false, // Tooltip näkyy vain, kun hiiri on elementin päällä
-            direction: 'auto', // Tooltipin suunta määräytyy automaattisesti
-            className: 'custom-tooltip' // Voit määritellä custom-luokan CSS-tyylien käyttöä varten
-        });
-    }
-}
-
-L.geoJSON(geoJsonData, {
-    onEachFeature: onEachFeature,
-    pointToLayer: pointToLayer, // Jos käytät custom pointToLayer-funktiota
-    style: style // Jos määrittelet tyylejä
-}).addTo(map);
 
     if (geoJsonData.features.length > 0) {
         map.fitBounds(window.searchResultsLayer.getBounds());
+    }
+}
+
+function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.nimi) {
+        layer.bindTooltip(feature.properties.nimi, {
+            permanent: false,
+            direction: 'auto',
+            className: 'custom-tooltip'
+        });
     }
 }
 
@@ -233,13 +143,13 @@ document.getElementById('searchInput').addEventListener('input', function() {
             return feature.properties.nimi.toLowerCase().includes(searchTerm);
         });
         displaySearchResults(filteredData);
-		piilotaVirheilmoitus();
+        piilotaVirheilmoitus();
     } else {
         document.getElementById('results').style.display = 'none';
     }
 });
 
-var currentLayer; // Määritellään currentLayer globaalilla tasolla
+var currentLayer;
 
 function displaySearchResults(features) {
     let resultsDiv = document.getElementById('results');
@@ -255,15 +165,12 @@ function displaySearchResults(features) {
 
             resultItem.addEventListener('click', function() {
                 if (currentLayer) {
-                    map.removeLayer(currentLayer); // Poista aiempi kerros, jos sellainen on
+                    map.removeLayer(currentLayer);
                 }
 
                 currentLayer = L.geoJSON(feature, {
-                    pointToLayer: function(feature, latlng) {
-                        return L.marker(latlng, {icon: customIcon});
-                    },
+                    pointToLayer: pointToLayer,
                     style: function(feature) {
-                        // Tässä esimerkissä oletetaan, että käytät samaa tyyliä kaikille featureille
                         return {
                             color: "blue",
                             weight: 8,
@@ -272,7 +179,6 @@ function displaySearchResults(features) {
                     }
                 }).addTo(map);
 
-                // Kohdista kartta valitun kohteen mukaan
                 if (feature.geometry.type === 'Point') {
                     var latLng = L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
                     map.setView(latLng, 12);
@@ -289,6 +195,36 @@ function displaySearchResults(features) {
     } else {
         resultsDiv.innerHTML = '<p>Ei hakutuloksia</p>';
         isSearchActive = false;
-       
+    }
+}
+
+function searchLocation(searchTerm) {
+    var searchTerm = document.getElementById('searchInput').value.trim();
+
+    if (searchTerm.length > 0 && globalGeoJsonData) {
+        var filteredData = {
+            type: "FeatureCollection",
+            features: globalGeoJsonData.features.filter(function(feature) {
+                return feature.properties.nimi.toLowerCase().includes(searchTerm.toLowerCase());
+            })
+        };
+
+        if (filteredData.features.length > 0) {
+            if (currentLayer) map.removeLayer(currentLayer);
+            currentLayer = L.geoJSON(filteredData, {
+                pointToLayer: pointToLayer,
+                style: style
+            }).addTo(map);
+            map.fitBounds(currentLayer.getBounds());
+            isSearchActive = true;
+            showCloseIcon();
+        } else {
+            console.error('Ei hakutuloksia hakusanalla: ' + searchTerm);
+            naytaVirheilmoitus('Ei hakutuloksia hakusanalla: ' + searchTerm);
+        }
+    } else {
+        console.error('Hakukenttä on tyhjä tai dataa ei ole vielä ladattu.');
+        naytaVirheilmoitus('Hakukenttä on tyhjä tai dataa ei ole vielä ladattu.');
+        showMagnifierIcon();
     }
 }
