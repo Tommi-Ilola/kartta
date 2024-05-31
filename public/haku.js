6021c6fae45de91b4c416626efaefc9651d420a6
Tommi-Ilola <148003799+Tommi-Ilola@users.noreply.github.com>
2024-05-31 15:30:02 +0300

    Update haku.js

--- public/haku.js
+++ public/haku.js
@@ -1,7 +1,6 @@
-var geojsonUrl = 'https://rata.digitraffic.fi/infra-api/0.7/tunnelit.geojson?';
-var anotherGeojsonUrl = 'https://rata.digitraffic.fi/infra-api/0.7/sillat.geojson?time=2024-02-22T06%3A53%3A28Z%2F2024-02-22T06%3A53%3A28Z';
-var thirdGeojsonUrl = 'https://rata.digitraffic.fi/infra-api/0.8/tasoristeykset.geojson?srsName=crs%3A84&time=2024-05-24T14%3A26%3A22Z%2F2024-05-24T15%3A26%3A22Z';
-
+var geojsonUrl = 'tunnelit.geojson';
+var anotherGeojsonUrl = 'sillat.geojson';
+var thirdGeojsonUrl = 'tasoristeykset.geojson';
 var SAGeojsonUrl = 'SA.geojson';
 var VKGeojsonUrl = 'VK.geojson';
 
